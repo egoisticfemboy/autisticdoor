b08f06eb5baa84b565e4e8f747cae89ea7041e15
egoisticfemboy <egoisticdoor@gmail.com>
2024-07-10 06:45:19 +0000

    edited the cooler version :3

--- ego_CoolerVersion.py
+++ ego_CoolerVersion.py
@@ -2,16 +2,16 @@
 import csv
 
 f = "Wetterdaten24.06.2024-30.06.2024.csv"
-data = []
+d = []
 
 try:
     with open(f, mode='r', newline='', encoding='utf-8') as file:
         reader = csv.reader(file, delimiter=';')
         header = next(reader)
-        data = list(reader)
+        d = list(reader)
 except FileNotFoundError:
     print(f"The file {f} could not be found. Please check the file name and if it exist")
 except csv.Error as e:
     print(f"Error at reading the CSV file: {e}")
 
-print(data)
+print(d)
